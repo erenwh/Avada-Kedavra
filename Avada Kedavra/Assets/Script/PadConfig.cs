a11bc36d7c5ce5c6bc17d10cfce17ba50c4ac7ac
rhbrian <brhee@purdue.edu>
2018-04-24 02:36:38 -0400

    added stats menu

--- Avada Kedavra/Assets/Script/PadConfig.cs
+++ Avada Kedavra/Assets/Script/PadConfig.cs
@@ -5,12 +5,16 @@ using Valve.VR;
 
 [RequireComponent(typeof(SteamVR_TrackedObject))]
 public class PadConfig : MonoBehaviour {
-
-	SteamVR_TrackedObject trackedObj;
+    public GameObject Menu;
+    private SteamVR_TrackedController controller;
+    public GameObject ControllerLeft;
+    SteamVR_TrackedObject trackedObj;
 	SteamVR_Controller.Device device;
 	public GameObject wand;
 	public GameObject scoreManager;
-	public bool isLeft;
+    public GameObject PlayerHealth;
+    public bool isLeft;
+    private bool menuTrack;
 
 	void Awake() {
 		trackedObj = GetComponent<SteamVR_TrackedObject>();
@@ -18,14 +22,36 @@ public class PadConfig : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        menuTrack = false;
+        Menu.SetActive(false);
+        controller = ControllerLeft.GetComponent<SteamVR_TrackedController>();
+        controller.MenuButtonClicked += MBC;
+    }
 
 	void FixedUpdate() {
 		device = SteamVR_Controller.Input((int)trackedObj.index);
 	}
-	
-	IEnumerator DamageCoroutine() {
+
+    private void MBC(object sender, ClickedEventArgs e)
+    {
+        MenuUp();
+    }
+
+    private void MenuUp()
+    {
+        if (menuTrack == false)
+        {
+            Menu.SetActive(true);
+            menuTrack = true;
+        }
+        else if (menuTrack == true)
+        {
+            Menu.SetActive(false);
+            menuTrack = false;
+        }
+    }
+
+    IEnumerator DamageCoroutine() {
 		//yield return new WaitForSeconds(.5f);
 		if (isLeft == false) {
 			while (scoreManager.GetComponent<Score>().score > 0) {
@@ -37,8 +63,23 @@ public class PadConfig : MonoBehaviour {
 		yield return new WaitForSeconds(.5f);
 	}
 
-	// Update is called once per frame
-	void Update () {
+    IEnumerator HealthCoroutine()
+    {
+        //yield return new WaitForSeconds(.5f);
+        if (isLeft == false)
+        {
+            while (scoreManager.GetComponent<Score>().score > 0)
+            {
+                scoreManager.GetComponent<Score>().score -= 1;
+                PlayerHealth.GetComponent<Target>().health += 1;
+                break;
+            }
+        }
+        yield return new WaitForSeconds(.5f);
+    }
+
+    // Update is called once per frame
+    void Update () {
 		if (device.GetPress(SteamVR_Controller.ButtonMask.Touchpad))
 		{
 			Vector2 touchpad = (device.GetAxis(Valve.VR.EVRButtonId.k_EButton_Axis0));
@@ -56,9 +97,9 @@ public class PadConfig : MonoBehaviour {
 
 			if (touchpad.x > 0.7f)
 			{
-				// pressing right
+                StartCoroutine(HealthCoroutine());
 
-			}
+            }
 
 			else if (touchpad.x < -0.7f)
 			{
