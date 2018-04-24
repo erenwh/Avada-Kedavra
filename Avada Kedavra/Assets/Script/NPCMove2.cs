652eb1e7b4e98562fd8da3ce0ff64586f3b38882
Han Wang <erenwh@gmail.com>
2018-04-24 03:04:59 -0400

    AI done

--- Avada Kedavra/Assets/Script/NPCMove2.cs
+++ Avada Kedavra/Assets/Script/NPCMove2.cs
@@ -1,16 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class NPCMove2 : MonoBehaviour {
+    //public Transform player;
     private Transform _destination;
+    private Animator anim;
 
-    UnityEngine.AI.NavMeshAgent _navMeshAgent;
+    NavMeshAgent _navMeshAgent;
+
+    public bool startAttack = false;
 
     // Use this for initialization
     void Start()
     {
-        _navMeshAgent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
+
+        anim = GetComponent<Animator>();
+
+        _navMeshAgent = this.GetComponent<NavMeshAgent>();
 
         if (_navMeshAgent == null)
         {
@@ -21,14 +29,40 @@ public class NPCMove2 : MonoBehaviour {
             SetDestination();
         }
     }
+
+    void Update()
+    {
+        Vector3 targetVector = _destination.transform.position;
+        if (Vector3.Distance(targetVector, this.transform.position) < 5)
+        {
+
+            startAttack = true;
+            Vector3 direction = _destination.position - this.transform.position;
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation,
+                                                Quaternion.LookRotation(direction), 0.1f);
+            anim.SetBool("isIdle", false);
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isAttacking", true);
+
+        }
+    }
+
+
     private void SetDestination()
     {
         _destination = GameObject.FindGameObjectWithTag("Target2").transform;
+
+
         if (_destination != null)
         {
-
             Vector3 targetVector = _destination.transform.position;
             _navMeshAgent.SetDestination(targetVector);
+            anim.SetBool("isIdle", false);
+            anim.SetBool("isWalking", true);
+            anim.SetBool("isAttacking", false);
         }
+
+
+
     }
 }
---
+++ Avada Kedavra/Assets/Slashed.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slashed : MonoBehaviour {
+
+
+    private void OnTriggerEnter(Collider c)
+    {
+        Debug.Log("FUCKYOU");
+    }
+}
